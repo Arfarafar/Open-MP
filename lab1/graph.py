c1786a84cdc20c2e8c97f682e4eae29ba9f51cb8
Arfarafar <andrew2001@bk.ru>
2022-12-13 19:44:02 +0300

    add graphics

--- lab1/graph.py
+++ lab1/graph.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 
 x, y = np.loadtxt("naive_many.txt", delimiter=' ', unpack=True)
 
+for i in range(len(x)):
+    y[i]=8.15/y[i]
 
 fig, ax = plt.subplots()
 ax.scatter(x, y, color = "orange",  label = "")
-plt.xticks([i for i in range(21)])
+plt.xticks([i for i in range(22)])
     
 ax.set_xlabel("Treads")
-ax.set_ylabel("Time")
+ax.set_ylabel("Acceleration")
 ax.grid()
-ax.legend()
 plt.show()
 
